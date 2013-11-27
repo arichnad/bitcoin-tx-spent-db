0f4f588a28aea6e87275abdbd6d9833c6cc36ef9
adrian <adrian-sw@aporter.org>
2013-11-27 19:33:17 +0000

    minor:  change a few of the names

--- colorer-b.js
+++ colorer-b.js
@@ -1,50 +1,50 @@
-var txdatacache = {};
+var transactionCache = {};
 var coinbaseHash = '0000000000000000000000000000000000000000000000000000000000000000';
 var defs = {};
+var transactionSpentCache = {};
 
 
-function getTransaction(txHash, callback) {
-    var data = txdatacache[txHash];
+
+function getTransaction(transactionHash, callback) {
+    var data = transactionCache[transactionHash];
     if (data)
         callback(data);
     else
     $.ajax({
-        url: "/rawtx/" + txHash,
+        url: "/rawtx/" + transactionHash,
         dataType: "json"
     }).done(function (data) {
-        txdatacache[txHash] = data;
+        transactionCache[transactionHash] = data;
         callback(data);
-    }).fail(function (what, thefuck) { alert('fail:' + what + ' ' + thefuck);});
+    }).fail(function (jqXHR, textStatus, errorThrown) { alert('fail:' + jqXHR + ' ' + textStatus);});
 }
 
-var txspentcache = {};
-
-function getWhoSpends(txHash, outIndex, callback) {
-    var k = outIndex + " " + txHash;
-    var data = txspentcache[k];
+function getWhoSpends(transactionHash, outIndex, callback) {
+    var k = outIndex + " " + transactionHash;
+    var data = transactionSpentCache[k];
     if (data) callback(data);
     else
     $.ajax({
-        url: "/spends/" + txHash,
+        url: "/spends/" + transactionHash,
         dataType: "json"
     }).done(function (data) {
-        txspentcache[k] = data[outIndex];
+        transactionSpentCache[k] = data[outIndex];
         callback(data);
-    }).fail(function (what, thefuck) { alert('fail:' + what + ' ' + thefuck);});
+    }).fail(function (jqXHR, textStatus, errorThrown) { alert('fail:' + jqXHR + ' ' + textStatus);});
 }
 
-function getSpent(txHash, callback) {
-    var k = txHash;
-    var data = txspentcache[k];
+function getSpent(transactionHash, callback) {
+    var k = transactionHash;
+    var data = transactionSpentCache[k];
     if (data) callback(data);
     else
     $.ajax({
-        url: "/spends/" + txHash,
+        url: "/spends/" + transactionHash,
         dataType: "json"
     }).done(function (data) {
-        txspentcache[k] = data;
+        transactionSpentCache[k] = data;
         callback(data);
-    }).fail(function (what, thefuck) { alert('fail:' + what + ' ' + thefuck);});
+    }).fail(function (jqXHR, textStatus, errorThrown) { alert('fail:' + jqXHR + ' ' + textStatus);});
 }
 
 
@@ -67,28 +67,28 @@ function btcToSatoshi(s) {
 // convert the input and output values from BTC to Satoshis
 // If not, then floating point rounding errors will propagate
 // and cause the matching algorithm to give wrong results
-function fixData(tx) {
-    tx.out.forEach(function(o) {
+function fixData(transaction) {
+    transaction.out.forEach(function(o) {
         o.value = btcToSatoshi(o.value);
     });
 
-    tx.in.forEach(function(i) {
+    transaction.in.forEach(function(i) {
         i.value = btcToSatoshi(i.prev_out.value);
     });
 }
 
-function matchInputs(tx) {
+function matchInputs(transaction) {
     var inputs = [0];
     var outputs = [];
     var matching = [];
 
     // create prefix sums
-    tx.out.reduce(function(prev, curr) {
+    transaction.out.reduce(function(prev, curr) {
         prev += curr.value;            
         outputs.push(prev);
         return prev;
     }, 0);
-    tx.in.reduce(function(prev, curr) {            
+    transaction.in.reduce(function(prev, curr) {            
         prev += curr.value
         inputs.push(prev);
         return prev;
@@ -113,14 +113,14 @@ function matchInputs(tx) {
     return matching;
 }
 
-function getMatching(txHash, callback) {
-    getTransaction(txHash, function(tx) {
-        callback(matchInputs(tx));
+function getMatching(transactionHash, callback) {
+    getTransaction(transactionHash, function(transaction) {
+        callback(matchInputs(transaction));
     });
 }
 
-function getColorByDefinition(txHash, outputIndex) {
-    var definition = defs[txHash];
+function getColorByDefinition(transactionHash, outputIndex) {
+    var definition = defs[transactionHash];
     
     if(definition === undefined || definition.outputIndex != outputIndex) {
         return 'Unknown';
@@ -128,14 +128,14 @@ function getColorByDefinition(txHash, outputIndex) {
     return definition.name;
 }
 
-function getColor(txHash, outputIdx, callback) {
+function getColor(transactionHash, outputIdx, callback) {
 
    function Helper(hash, idx, cb) {
         this.color = 'Unknown';
 
         // put initial transaction output in queue
         this.queue = [{
-            txHash: hash,
+            transactionHash: hash,
             index: idx
         }];
 
@@ -150,7 +150,7 @@ function getColor(txHash, outputIdx, callback) {
             }
 
             var currentOutput = this.queue.pop();
-            var currentHash = currentOutput['txHash'];
+            var currentHash = currentOutput['transactionHash'];
             var currentOutIdx = currentOutput['index'];
 
 
@@ -178,25 +178,25 @@ function getColor(txHash, outputIdx, callback) {
             // otherwise, get the transaction, and add
             // the matching inputs to the queue
             else {
-                getTransaction(currentHash, function(tx) {
+                getTransaction(currentHash, function(transaction) {
                     // is it coinbase, then we can't go any deeper, so it isn't colored
-                    if(tx.in[0].prev_out.hash === coinbaseHash) {
+                    if(transaction.in[0].prev_out.hash === coinbaseHash) {
                         this.callback('None');
                     }
 
                     else {
                         // fix the in and out values to be integers rather than decimals
-                        fixData(tx);
+                        fixData(transaction);
 
-                        var matching = matchInputs(tx);
+                        var matching = matchInputs(transaction);
 
 
                         // add the matching inputs to the queue
                         matching[currentOutIdx].reverse().forEach(function(inIdx) {
-                            var input = tx.in[inIdx];
+                            var input = transaction.in[inIdx];
 
                             this.queue.push({
-                                txHash: input.prev_out.hash,
+                                transactionHash: input.prev_out.hash,
                                 index: input.prev_out.n
                             });
                         }.bind(this));
@@ -213,58 +213,58 @@ function getColor(txHash, outputIdx, callback) {
     };
 
 
-    var helper = new Helper(txHash, outputIdx, callback);
+    var helper = new Helper(transactionHash, outputIdx, callback);
 }
 
 
 function test(i, callback) {
     var testVectors = [
         {
-            txHash: '3a60b70d425405f3e45f9ed93c30ca62b2a97e692f305836af38a524997dd01d',
+            transactionHash: '3a60b70d425405f3e45f9ed93c30ca62b2a97e692f305836af38a524997dd01d',
             outputIdx: 0,
             color: 'None'
         },{
-            txHash: 'c1d8d2fb75da30b7b61e109e70599c0187906e7610fe6b12c58eecc3062d1da5',
+            transactionHash: 'c1d8d2fb75da30b7b61e109e70599c0187906e7610fe6b12c58eecc3062d1da5',
             outputIdx: 0,
             color: 'Red'
         },{
-            txHash: '8f6c8751f39357cd42af97a67301127d497597ae699ad0670b4f649bd9e39abf',
+            transactionHash: '8f6c8751f39357cd42af97a67301127d497597ae699ad0670b4f649bd9e39abf',
             outputIdx: 0,
             color: 'Red'
         },{
-            txHash: 'f50f29906ce306be3fc06df74cc6a4ee151053c2621af8f449b9f62d86cf0647',
+            transactionHash: 'f50f29906ce306be3fc06df74cc6a4ee151053c2621af8f449b9f62d86cf0647',
             outputIdx: 0,
             color: 'Blue'
         },{
-            txHash: '7e40d2f414558be60481cbb976e78f2589bc6a9f04f38836c18ed3d10510dce5',
+            transactionHash: '7e40d2f414558be60481cbb976e78f2589bc6a9f04f38836c18ed3d10510dce5',
             outputIdx: 0,
             color: 'Blue'
         },{
-            txHash: '4b60bb49734d6e26d798d685f76a409a5360aeddfddcb48102a7c7ec07243498',
+            transactionHash: '4b60bb49734d6e26d798d685f76a409a5360aeddfddcb48102a7c7ec07243498',
             outputIdx: 0,
             color: 'Red'
         },{
-            txHash: '342f119db7f9989f594d0f27e37bb5d652a3093f170de928b9ab7eed410f0bd1',
+            transactionHash: '342f119db7f9989f594d0f27e37bb5d652a3093f170de928b9ab7eed410f0bd1',
             outputIdx: 0,
             color: 'None'
         },{
-            txHash: 'bd34141daf5138f62723009666b013e2682ac75a4264f088e75dbd6083fa2dba',
+            transactionHash: 'bd34141daf5138f62723009666b013e2682ac75a4264f088e75dbd6083fa2dba',
             outputIdx: 0,
             color: 'Blue'
         },{
-            txHash: 'bd34141daf5138f62723009666b013e2682ac75a4264f088e75dbd6083fa2dba',
+            transactionHash: 'bd34141daf5138f62723009666b013e2682ac75a4264f088e75dbd6083fa2dba',
             outputIdx: 1,
             color: 'None'
         },{
-            txHash: '36af9510f65204ec5532ee62d3785584dc42a964013f4d40cfb8b94d27b30aa1',
+            transactionHash: '36af9510f65204ec5532ee62d3785584dc42a964013f4d40cfb8b94d27b30aa1',
             outputIdx: 0,
             color: 'Red'
         },{
-            txHash: '741a53bf925510b67dc0d69f33eb2ad92e0a284a3172d4e82e2a145707935b3e',
+            transactionHash: '741a53bf925510b67dc0d69f33eb2ad92e0a284a3172d4e82e2a145707935b3e',
             outputIdx: 0,
             color: 'Red'
         },{
-            txHash: '741a53bf925510b67dc0d69f33eb2ad92e0a284a3172d4e82e2a145707935b3e',
+            transactionHash: '741a53bf925510b67dc0d69f33eb2ad92e0a284a3172d4e82e2a145707935b3e',
             outputIdx: 1,
             color: 'Red'
         }
@@ -272,7 +272,7 @@ function test(i, callback) {
 
     var test = testVectors[i];
 
-    getColor(test.txHash, test.outputIdx, function(color) {
+    getColor(test.transactionHash, test.outputIdx, function(color) {
         console.log(i, color, test.color);
         callback();
     });
@@ -311,39 +311,39 @@ function testAll() {
 
 // testAll();
 
-function rendertx(tx, spent) {
+function renderTransaction(transaction, spent) {
     
     if (spent === undefined) {
-        return getSpent(tx.hash, function (spent) {
-            rendertx(tx, spent);
+        return getSpent(transaction.hash, function (spent) {
+            renderTransaction(transaction, spent);
         });
     }
 
-    fixData(tx);
+    fixData(transaction);
 
-    var ospenttx = {};
+    var outputSpentTransaction = {};
     spent.forEach(function (o) {
-        ospenttx[o[0]] = o[1];
+        outputSpentTransaction[o[0]] = o[1];
     });
     
-    var $box = $('#txbox');
+    var $box = $('#transactionBox');
     $box.empty();
     
     var cereal = 0;
 
-    $box.append("<p><strong>Transaction</strong> " + tx.hash + "</p>");
+    $box.append("<p><strong>Transaction</strong> " + transaction.hash + "</p>");
 
     $box.append("<p><strong>Inputs:</strong></p>");
 
-    tx.in.forEach(function(i) {
+    transaction.in.forEach(function(i) {
         var spanid = "col" + cereal; cereal ++;
 
         $p = $("<p>value: " + i.value + ", color: <span id='" + spanid + "'>?</span> </p>");
 
         if (i.prev_out.hash != coinbaseHash) {
             $a = $("<a href='#'>" + i.prev_out.hash + " " + i.prev_out.n + "</a>");
-            (function (txhash) {
-                $a.click(function () { goTransaction(txhash); });
+            (function (transactionHash) {
+                $a.click(function () { goTransaction(transactionHash); });
             })(i.prev_out.hash);
             $t = $("<small>Prev: </small>")
             $t.append($a);
@@ -366,16 +366,16 @@ function rendertx(tx, spent) {
 
     $box.append("<p><strong>Outputs:</strong></p>");
 
-    tx.out.forEach(function(o, oi) {
+    transaction.out.forEach(function(o, oi) {
         var spanid = "col" + cereal; cereal ++;
 
         $p = $("<p>value: " + o.value + ", color: <span id='" + spanid + "'>?</span>   </p>");
-        if (ospenttx[oi]) {
-            var txhash = ospenttx[oi];
-            $a = $("<a href='#'>" + txhash + "</a>");
-            (function (txhash) {
-                $a.click(function () { goTransaction(txhash); });
-            })(txhash);
+        if (outputSpentTransaction[oi]) {
+            var transactionHash = outputSpentTransaction[oi];
+            $a = $("<a href='#'>" + transactionHash + "</a>");
+            (function (transactionHash) {
+                $a.click(function () { goTransaction(transactionHash); });
+            })(transactionHash);
             $t = $("<small>Spent: </small>")
             $t.append($a);
             $p.append($t);
@@ -384,7 +384,7 @@ function rendertx(tx, spent) {
 
         $box.append($p);
         (function (spanid) {
-            getColor(tx.hash, oi, function (color) {
+            getColor(transaction.hash, oi, function (color) {
                 $("#" + spanid).text(color);
             });
         })(spanid);
